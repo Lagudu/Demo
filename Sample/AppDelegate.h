88408f5d1e4a5c283a627d3eedcfeb8545dd095a
Etisbew <Etisbew@Etisbews-iMac.local>
2015-12-28 15:56:26 +0530

    First Project

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Etisbew on 28/12/15.
+//  Copyright (c) 2015 Etisbew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Etisbew on 28/12/15.
+//  Copyright (c) 2015 Etisbew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
